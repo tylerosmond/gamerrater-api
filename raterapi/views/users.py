05c1f930cf96daa051d0e1ae7550e1ded9d21a77
Tyler Osmond <140542331+tylerosmond@users.noreply.github.com>
2024-09-19 14:17:19 -0500

    test

--- raterapi/views/users.py
+++ raterapi/views/users.py
@@ -11,8 +11,8 @@ from django.contrib.auth import authenticate
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'first_name', 'last_name']
-        extra_kwargs = {'password': {'write_only': True}} 
+        fields = ["id", "username", "password", "first_name", "last_name"]
+        extra_kwargs = {"password": {"write_only": True}}
         # password is required in the request, but won't be sent back in the response
 
 
@@ -20,29 +20,34 @@ class UserViewSet(viewsets.ViewSet):
     queryset = User.objects.all()
     permission_classes = [permissions.AllowAny]
 
-    @action(detail=False, methods=['post'], url_path='register')
+    @action(detail=False, methods=["post"], url_path="register")
     def register_account(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = User.objects.create_user(
-                username=serializer.validated_data['username'],
-                first_name=serializer.validated_data['first_name'],
-                last_name=serializer.validated_data['last_name'],
-                password=serializer.validated_data['password']
+                username=serializer.validated_data["username"],
+                first_name=serializer.validated_data["first_name"],
+                last_name=serializer.validated_data["last_name"],
+                password=serializer.validated_data["password"],
             )
             token, created = Token.objects.get_or_create(user=user)
             return Response({"token": token.key}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=False, methods=['post'], url_path='login')
+    @action(detail=False, methods=["post"], url_path="login")
     def user_login(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
+        username = request.data.get("username")
+        password = request.data.get("password")
 
         user = authenticate(username=username, password=password)
 
         if user:
             token = Token.objects.get(user=user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
         else:
-            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Invalid Credentials"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+# test
